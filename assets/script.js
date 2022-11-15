2c7ac4ee727753252a0e4c6d6ffb19486dca7bd0
KhoaLe <91020137+leykhoa@users.noreply.github.com>
2022-11-16 00:24:42 +0700

    WFH

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,44 @@
-$(".slider-center").slick({
-    centerMode: true,
-    centerPadding: "16px",
-    slidesToShow: 1.5,
-    mobileFirst: true,
-    arrows: false,
-    dots: true,
-    cssEase: "ease",
-    initialSlide: 0,
-    responsive: [
-        {
-            breakpoint: 768,
-            settings: {
-                arrows: false,
-                centerMode: true,
-                centerPadding: "20px",
-                slidesToShow: 3,
-            },
-        },
-    ],
+$('.slider-center').slick({
+	centerMode: true,
+	centerPadding: '16px',
+	slidesToShow: 1.67,
+	mobileFirst: true,
+	arrows: false,
+	dots: true,
+	respondTo: 'slider',
+	swipeToSlide: true,
+	autoPlay: true,
+	speed: 500,
+	responsive: [
+		{
+			breakpoint: 768,
+			settings: {
+				arrows: false,
+				centerMode: true,
+				centerPadding: '20px',
+				slidesToShow: 3,
+			},
+		},
+	],
 });
+
+//my script
+
+function handleToggle(element) {
+	const menu = document.querySelector('.navbar__menu');
+	const toggleClose = document.querySelector('#toggle-close');
+	const toggleOpen = document.querySelector('#toggle-open');
+	if (element.id === 'toggle-open') {
+		toggleOpen.classList.add('display-none');
+		toggleClose.classList.remove('display-none');
+	} else {
+		toggleClose.classList.add('display-none');
+		toggleOpen.classList.remove('display-none');
+	}
+
+	if (menu.style.display !== 'flex') {
+		menu.style.display = 'flex';
+	} else {
+		menu.style.display = 'none';
+	}
+}
